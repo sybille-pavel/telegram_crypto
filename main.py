56ed474f753781c054e5cf5b380d0ce699db2b38
sybille <sybille.pavel@gmail.com>
2022-01-12 00:41:53 +0300

    первые шаги

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-#import eel
+import eel
 import TelegramManager
 
-TelegramManager.send_file("vitalyi_shpagin", "web/main.html")
 
-exit()
+@eel.expose
+def get_chats():
+    return TelegramManager.get_chats()
+
+
+@eel.expose
+def send_message(id, text):
+    print(id, text)
+
 
 eel.init("web")
 eel.start("main.html", size=(1200, 700))
