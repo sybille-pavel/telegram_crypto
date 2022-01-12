ca6071c781e496dff159cbc2771679a2a4bbb482
vitalya <vitalijspagin1@gmail.com>
2022-01-12 05:46:18 +0300

    чат инфо

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import RSATelegram
 import eel
 import TelegramManager
+import pprint as pp
+
 
 @eel.expose
 def get_chats():
@@ -25,12 +27,29 @@ def send_public_key(id):
 
 @eel.expose
 def get_messages(id):
-    return TelegramManager.get_messages(int(id), limit=5)
+    res = TelegramManager.get_messages(int(id), limit=5)
+
+    for info in res:
+        if info is None: continue
+
+        if info["type"] == "document":
+            name = info["value"]
+            name = name.replace(" ", ".")
+            name = name.split("\\")[1]
+            name = name.split(".")
+
+            if name[0] == "receiver" and name[-1] == "pem":
+                crypto = RSATelegram.RSAMessages(id)
+                cryptov = crypto.setCompanionPublicKey(open(info['value'].replace("\\", "/")).read())
+
+    return res
+
 
 @eel.expose
 def get_chat_info(id):
     crypto = RSATelegram.RSAMessages(id)
     return crypto.getChatInfo()
+
 
 @eel.expose
 def get_companion_public_key(id):
@@ -40,4 +59,3 @@ def get_companion_public_key(id):
 
 eel.init("web")
 eel.start("main.html", size=(1200, 700))
-
